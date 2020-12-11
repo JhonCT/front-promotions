38e196aaeb6133710c6452c0a8af91c5d4304efb
Angel E <as.sosaramos@gmail.com>
2020-12-11 18:59:51 -0500

    build: entornos dev, uat y live

--- scripts.dev.bash
+++ scripts.dev.bash
@@ -9,6 +9,6 @@ cd /home/assosaramos/operator-admin-app-no-wl/ \
 && git pull \
 && git --no-pager show --summary \
 && docker image prune -f \
-&& docker-compose --project-name casinovip365_ --file docker-compose.yaml build --force-rm dev.ope.shop.desktop \
-&& docker-compose --project-name casinovip365_ --file docker-compose.yaml rm -f dev.ope.shop.desktop \
-&& docker-compose --project-name casinovip365_ --file docker-compose.yaml up -d --no-deps dev.ope.shop.desktop
+&& docker-compose --project-name casinovip365_ --file docker-compose.dev.yaml build --force-rm dev.ope.shop.desktop \
+&& docker-compose --project-name casinovip365_ --file docker-compose.dev.yaml rm -f dev.ope.shop.desktop \
+&& docker-compose --project-name casinovip365_ --file docker-compose.dev.yaml up -d --no-deps dev.ope.shop.desktop
--- scripts.uat.bash
+++ scripts.uat.bash
@@ -8,7 +8,7 @@ cd /home/assosaramos/operator-admin-app-no-wl/ \
 && git pull \
 && git --no-pager show --summary \
 && docker image prune -f \
-&& docker-compose --project-name casinovip365_ --file docker-compose.yaml build --force-rm uat.ope.shop.desktop \
-&& docker-compose --project-name casinovip365_ --file docker-compose.yaml rm -f uat.ope.shop.desktop \
-&& docker-compose --project-name casinovip365_ --file docker-compose.yaml up -d --no-deps uat.ope.shop.desktop
+&& docker-compose --project-name casinovip365_ --file docker-compose.uat.yaml build --force-rm uat.ope.shop.desktop \
+&& docker-compose --project-name casinovip365_ --file docker-compose.uat.yaml rm -f uat.ope.shop.desktop \
+&& docker-compose --project-name casinovip365_ --file docker-compose.uat.yaml up -d --no-deps uat.ope.shop.desktop
 
